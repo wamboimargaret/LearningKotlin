f03e5e91f5d5a78fd68984bc38d4f3983afda83a
student <margarettwmboi@gmail.com>
2023-02-06 11:08:45 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    var fullName: String = "MargaretWamboi"
+    println(fullName)
+    var age: Byte = 21
+    println(age)
+    var salary: Int = 500450
+    println(salary)
+    val gender: Boolean = true
+    println(gender)
+    val citizen: Boolean = false
+    println(citizen)
+    var text: String = "Confirm you have received 500 ksh from KCB bank"
+    println(text)
+
+
+}
